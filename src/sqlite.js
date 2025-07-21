a1f7bed51a2fe2cd2ff127d5ac78924044318279
Matt Miller <thisismattmiller@gmail.com>
2025-07-20 21:03:19 -0400

    Add files via upload

---
+++ src/sqlite.js
@@ -0,0 +1,146 @@
+/**
+ * Module handles database management
+ *
+ * Server API calls the methods in here to query and update the SQLite database
+ */
+
+// Utilities we need
+const fs = require("fs");
+
+// Initialize the database
+const dbFile = "./.data/edge_notch_cards.db";
+const exists = fs.existsSync(dbFile);
+const sqlite3 = require("sqlite3").verbose();
+const dbWrapper = require("sqlite");
+let db;
+
+/* 
+We're using the sqlite wrapper so that we can make async / await connections
+- https://www.npmjs.com/package/sqlite
+*/
+dbWrapper
+  .open({
+    filename: dbFile,
+    driver: sqlite3.Database
+  })
+  .then(async dBase => {
+    db = dBase;
+  });
+
+// Our server script will call these methods to connect to the db
+module.exports = {
+  
+  /**
+   * Get the options in the database
+   *
+   * Return everything in the Choices table
+   * Throw an error in case of db connection issues
+   */
+  getCards: async () => {
+    // We use a try catch block in case of db errors
+    try {
+      return await db.all("SELECT * from cards");
+    } catch (dbError) {
+      // Database connection error
+      console.error(dbError);
+    }
+  },
+  
+  touchCard: (id) =>{
+    db.run(
+      "UPDATE cards SET touch = ? WHERE id = ?",
+      [Math.floor(Date.now() / 1000), id]
+    );
+
+  },
+  
+  saveCard: (id,namesString,isComplete) =>{
+    if (isComplete){
+      db.run(
+        "UPDATE cards SET complete = 1, names = ? WHERE id = ?",
+        [namesString, id]
+      );
+    }else{
+      db.run(
+        "UPDATE cards SET complete = 0, names = ? WHERE id = ?",
+        [namesString, id]
+      );      
+      
+    }
+
+
+  },
+
+  /**
+   * Process a user vote
+   *
+   * Receive the user vote string from server
+   * Add a log entry
+   * Find and update the chosen option
+   * Return the updated list of votes
+   */
+  processVote: async vote => {
+    // Insert new Log table entry indicating the user choice and timestamp
+    try {
+      // Check the vote is valid
+      const option = await db.all(
+        "SELECT * from Choices WHERE language = ?",
+        vote
+      );
+      if (option.length > 0) {
+        // Build the user data from the front-end and the current time into the sql query
+        await db.run("INSERT INTO Log (choice, time) VALUES (?, ?)", [
+          vote,
+          new Date().toISOString()
+        ]);
+
+        // Update the number of times the choice has been picked by adding one to it
+        await db.run(
+          "UPDATE Choices SET picks = picks + 1 WHERE language = ?",
+          vote
+        );
+      }
+
+      // Return the choices so far - page will build these into a chart
+      return await db.all("SELECT * from Choices");
+    } catch (dbError) {
+      console.error(dbError);
+    }
+  },
+
+  /**
+   * Get logs
+   *
+   * Return choice and time fields from all records in the Log table
+   */
+  getLogs: async () => {
+    // Return most recent 20
+    try {
+      // Return the array of log entries to admin page
+      return await db.all("SELECT * from Log ORDER BY time DESC LIMIT 20");
+    } catch (dbError) {
+      console.error(dbError);
+    }
+  },
+
+  /**
+   * Clear logs and reset votes
+   *
+   * Destroy everything in Log table
+   * Reset votes in Choices table to zero
+   */
+  clearHistory: async () => {
+    try {
+      // Delete the logs
+      await db.run("DELETE from Log");
+
+      // Reset the vote numbers
+      await db.run("UPDATE Choices SET picks = 0");
+
+      // Return empty array
+      return [];
+    } catch (dbError) {
+      console.error(dbError);
+    }
+  }
+};
